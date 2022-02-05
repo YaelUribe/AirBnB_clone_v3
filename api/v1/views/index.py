a4d4c44640d0140518349e7c20ce7067e12b9e57
ele4327 <sebasgarzons88@gmail.com>
2022-02-05 16:14:30 -0600

    A endpoint that retrieves a number for each object

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,33 @@ module: index
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """returns a JSON file with status: ok"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """"""
+    dict_class = {
+        'amenity': Amenity,
+        'city': City,
+        'place': Place,
+        'review': Review,
+        'state': State,
+        'user': User
+    }
+
+    new_dict = {key: storage.count(value) for key, value in dict_class.items()}
+
+    return(new_dict)
