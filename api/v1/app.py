de83d374b9727f471e06e83584df413378187eb0
YaelUribe <yael3x@gmail.com>
2022-02-06 09:39:33 -0500

    Handling error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Status of api"""
 from logging import error
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,6 +14,16 @@ app.register_blueprint(app_views)
 def close_storage(self):
     """Close Storage"""
     storage.close()
+
+
+@app.errorhandler(404)  # Errors handle by code or exception class
+def nope(error):
+    """Handling error 404"""
+    nope = {"error": "Not found"}
+    return make_response(jsonify(nope), 404)
+    # This creates a response with a 404 error code
+    # by setting an additional header view
+
 
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
